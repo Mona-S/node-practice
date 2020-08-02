1a32c0e84c01045be51019c3b1662b71d1fc0729
Mona-S <forward2cm@gmail.com>
2020-08-02 16:24:46 +0000

    use body parser to parse the http request

--- calculator.js
+++ calculator.js
@@ -2,14 +2,20 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-app.use(bodyParser)
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.get("/", function(req,res){
     res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", function(req,res){
-    res.send("Thanks for posting");
+
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+
+    result = num1 + num2;
+
+    res.send("The result is " + result);
 });
 
 app.listen(3000, function(){
