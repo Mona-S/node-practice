08bc0c6b268f938d6e58be04860fea3007f8bfba
Mona-S <forward2cm@gmail.com>
2020-08-17 12:05:00 +0000

    set up the post request and sent the result

--- multiplyNum.js
+++ multiplyNum.js
@@ -8,15 +8,15 @@ app.get("/", function(req,res){
     res.sendFile(__dirname + "/multiplyNum.html");
 });
 
-// app.post("/", function(req,res){
+app.post("/", function(req,res){
 
-//     var weight = Number(req.body.Weight);
-//     var height = Number(req.body.Height);
+    var number1 = Number(req.body.number1);
+    var number2 = Number(req.body.number2);
 
-//     var result = weight + height;
+    var result = number1 * number2;
 
-//     res.send("Result is " + result);
-// });
+    res.send("Result is " + result);
+});
 
 app.listen(3000, function(){
     console.log("Server started");
