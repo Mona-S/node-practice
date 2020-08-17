5dbfea01e2c32f81f2238242f82fa41073ffd760
Mona-S <forward2cm@gmail.com>
2020-08-17 12:03:39 +0000

    started the server and set up the get request

--- multiplyNum.js
+++ multiplyNum.js
@@ -5,19 +5,18 @@ const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req,res){
-    res.sendFile(__dirname + "/bmiCalculatorNew.html");
+    res.sendFile(__dirname + "/multiplyNum.html");
 });
 
-app.post("/", function(req,res){
+// app.post("/", function(req,res){
 
-    var weight = Number(req.body.Weight);
-    var height = Number(req.body.Height);
+//     var weight = Number(req.body.Weight);
+//     var height = Number(req.body.Height);
 
-    var result = weight + height;
+//     var result = weight + height;
 
-
-    res.send("Result is " + result);
-});
+//     res.send("Result is " + result);
+// });
 
 app.listen(3000, function(){
     console.log("Server started");
