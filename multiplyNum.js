11fac00e829f30ca97805a3f0c23a8984ac52924
Mona-S <forward2cm@gmail.com>
2020-08-17 12:01:57 +0000

    created the index.html file

---
+++ multiplyNum.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname + "/bmiCalculatorNew.html");
+});
+
+app.post("/", function(req,res){
+
+    var weight = Number(req.body.Weight);
+    var height = Number(req.body.Height);
+
+    var result = weight + height;
+
+
+    res.send("Result is " + result);
+});
+
+app.listen(3000, function(){
+    console.log("Server started");
+});
